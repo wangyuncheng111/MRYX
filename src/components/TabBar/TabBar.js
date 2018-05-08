d413e4a41af5a87c6dc8be58d028305f28e3b7af
unknown <2376338970@qq.com>
2018-05-08 13:05:39 +0800

    666

--- src/components/TabBar/TabBar.js
+++ src/components/TabBar/TabBar.js
@@ -12,22 +12,22 @@ export default class TabBar extends React.Component {
             <div className='tab-bar'>
                 <li>
                     <NavLink to='/home'>
-                        <i className='iconfont icon-xingqiu'></i>
+                        <i className='iconfont icon-shouye1'></i>
                         首页</NavLink>
                 </li>
                 <li>
                     <NavLink to='/vip'>
-                        <i className='iconfont icon-react'></i>
+                        <i className='iconfont icon-huiyuan'></i>
                         会员</NavLink>
                 </li>
                 <li>
                     <NavLink to='/cart'>
-                        <i className='iconfont icon-wode_kecheng'></i>
+                        <i className='iconfont icon-gouwuche'></i>
                         购物车</NavLink>
                 </li>
                 <li>
                     <NavLink to='/my'>
-                        <i className='iconfont icon-wode_kecheng'></i>
+                        <i className='iconfont icon-iconfontgerenzhongxin'></i>
                         我的</NavLink>
                 </li>
             </div>
